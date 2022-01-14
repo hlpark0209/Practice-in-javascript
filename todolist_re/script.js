76572a571a7ff09afde5ae362b5f9bc0ad07d76f
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-15 02:29:16 +0900

    Update delete function

--- todolist_re/script.js
+++ todolist_re/script.js
@@ -5,6 +5,9 @@ const addBtn = document.querySelector('.addBtn');
 const list = document.querySelector('.list');
 
 //2. li 요소가 append
+// ⭐ id 값을 0부터 시작으로 설정 ⭐
+let id = 0;
+
 function appendList(){
     const value = input.value;
     const listLi = document.createElement('li');
@@ -15,15 +18,18 @@ function appendList(){
         <div class="text">
             <div class="todo">${value}</div>
             <button class="delete">
-                <i class="fas fa-trash"></i>
+                <i class="fas fa-trash" data-id=${id}></i>
             </button>
         </div>
         <div class="line"></div>
-        `
+        `;
+    id++;
     list.appendChild(listLi);
     input.value = "";
     }
+    return listLi;
 }
+
 
 //1. 추가 or enter 버튼 눌렀을때 
 addBtn.addEventListener('click', appendList);
@@ -35,3 +41,11 @@ document.addEventListener('keydown', () => {
 
 
 //3. 삭제기능
+list.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    console.log(id);
+    if( id ){
+        const del = document.querySelector(`.text[data-id="${id}"]`);
+        console.log(del);
+    }
+});
