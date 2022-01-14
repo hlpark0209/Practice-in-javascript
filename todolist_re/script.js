fb4d012ab7f9221920e23a30e15b02c9aa179bf8
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-15 01:34:10 +0900

    Remake todo list function
    html, css, js

--- todolist_re/script.js
+++ todolist_re/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.addBtn');
+const list = document.querySelector('.list');
+
+//2. li 요소가 append
+function appendList(){
+    const value = input.value;
+    const listLi = document.createElement('li');
+    if( value === "") {
+        alert(" Write you task !");
+    } else{
+    listLi.innerHTML = `
+        <div class="text">
+            <div class="todo">${value}</div>
+            <button class="delete">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>
+        <div class="line"></div>
+        `
+    list.appendChild(listLi);
+    input.value = "";
+    }
+}
+
+//1. 추가 or enter 버튼 눌렀을때 
+addBtn.addEventListener('click', appendList);
+document.addEventListener('keydown', () => {
+    if(window.event.keyCode === 13){
+        appendList();
+    }
+});
+
+
+//3. 삭제기능
