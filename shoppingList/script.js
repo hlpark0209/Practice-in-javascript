f4203736d234bf992aa5a5fa04c05bee60f0d584
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-20 21:27:46 +0900

    Update click event, filtering function

--- shoppingList/script.js
+++ shoppingList/script.js
@@ -14,6 +14,7 @@ function displayItems(items){
 
 }
 
+// 받아온 item들을 HTML태그로 생성
 function createHMTLString(item){
     return `
     <li>
@@ -26,11 +27,38 @@ function createHMTLString(item){
 }
 
 
+// 버튼클릭시 각 버튼의 정보에 맞는 아이템들을 filtering
+// items를 displayItems에 호출
+function onBtnClick(e, items){
+    const key = e.target.dataset.key;
+    const value = e.target.dataset.value;
+
+    if( key == null && value == null){
+        return;
+    }else{
+
+        displayItems(items.filter(item => item[key] === value));
+        console.log(value);
+    }
+}
 
 
+// 로고와 버튼 클릭 이벤트
+function setEventListeners(items){
+    const logo = document.querySelector('#logo');
+    const buttons = document.querySelector('#filter_btn');
+
+    logo.addEventListener("click", () => displayItems(items));
+    buttons.addEventListener("click", e => onBtnClick(e, items));
+
+}
+
+
+
+//함수호출
 loadItems()
 .then(items => {
     displayItems(items);
-    // setEventListeners(items)
+    setEventListeners(items);
 })
 .catch(console.log);
