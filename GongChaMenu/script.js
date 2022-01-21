35e70826bf27c1ad0753f9bbc22767698e3f925d
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-21 23:23:00 +0900

    Update filtering+cllick event

--- GongChaMenu/script.js
+++ GongChaMenu/script.js
@@ -5,35 +5,47 @@ let array =[];
 function loadItems(){
     return fetch('data/data.json')
     .then( response => response.json())
-    .then( item => { 
-        array = item;
-        console.log(array);
-    });
-    
+    .then( json => json.items);
+        
 }
 
 
-// // 받아온 데이터를 update
-// function displayItems(items){
-//     const menuContainer = document.querySelector(".menuContainer");
-//     // menuContainer.innerHTML = items;
+// // 받아온 데이터를 update 한 후 html로 변환
+function displayItems(items){
+    const menuContainer = document.querySelector(".menuContainer");
+    menuContainer.innerHTML = items.map( (item) => createHTML(item)).join('');
+        
+    function createHTML(e){
+        return `
+        <div class="menuThumb">
+            <img src="${e.image}" class="menuImg">
+            <p>${e.title}</p>
+        </div>
+        `;
+    }
+}
+
+// click event 적용
+
+function onclickBtn(e, items){
+    const key = e.target.dataset.key;
+    const value = e.target.dataset.value;
+
+    if( key == null ){
+        return;
+    } else{
+        displayItems(items.filter( item => item[key] === value));
+    }
+}
+
+
+
+
+function filterItems(items){
+    const btn = document.querySelector('.menuBtns');
+    btn.addEventListener('click', e => onclickBtn (e, items));
     
-//     const value = items.map( item => createHMTLString(item)).join('');
-    
-// }
-// function createHMTLString(item){
-
-// }
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -41,6 +53,7 @@ function loadItems(){
 
 loadItems()
 .then( items => {
-//     displayItems(items);
+    displayItems(items);
+    filterItems(items);
 })
-.catch(console.log('warning!'));
+.catch(console.log);
